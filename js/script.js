9d23c76f67d5d8861a472d6f311f9392013f877a
default <yunus.emre.guzel@mths.ca>
2025-03-18 16:51:28 +0000

    added the templete

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emre Guzel
+// Created on: March 7  2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
-function myButtonClicked() {
-  alert("Hello, World!")
+function calculateBtn() {
+  document.getElementById("perimeter").innerHTML = "<p>  Perimeter is:" + 2 * (5 + 3) + 'cm' + "</p>"
+  document.getElementById("area").innerHTML = "<p> Area is:" + 5 * 3 + 'cm² ' + "</p>"
 }
