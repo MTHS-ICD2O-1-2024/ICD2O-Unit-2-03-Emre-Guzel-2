690ba6b9fa66b46c56bd2c99bdbf1a4b998f6c55
default <yunus.emre.guzel@mths.ca>
2025-03-18 21:45:56 +0000

    changed the dates

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Emre Guzel
-// Created on: March 7  2025
+// Created on: March 18  2025
 // This file contains the JS functions for index.html
 
 function streetAddress() {
